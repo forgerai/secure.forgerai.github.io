978bad009c2d0c58d74c97a57a9317467dd8185c
Vaishnav S Menon <vaishnavsm@gmail.com>
2025-01-26 15:19:09 -0700

    change button to gradients

--- server/chunks/ssr/[root of the server]__fd2205._.js
+++ server/chunks/ssr/[root of the server]__fd2205._.js
@@ -332,7 +332,7 @@ const Hero = ()=>{
                     children: [
                         /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("button", {
                             onClick: hangon,
-                            className: "bg-orange-400 border-orange-400 hover:bg-orange-500 text-white py-2 px-8 rounded-sm transform duration-200 hover:px-16",
+                            className: "bg-gradient-to-r from-orange-400 to-orange-500 text-white py-2 px-8 rounded-sm transform duration-200 hover:px-16",
                             children: "Create A Space"
                         }, void 0, false, {
                             fileName: "[project]/src/app/consumer.tsx",
